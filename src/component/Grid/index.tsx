da976845692404e00e183a1afb3046b9b9e12202
Ypisds <thrbritto4@gmail.com>
2026-04-29 21:56:29 -0300

    feat(Agent): adição do componente Agent e integração com o Grid

--- src/component/Grid/index.tsx
+++ src/component/Grid/index.tsx
@@ -1,9 +1,12 @@
 import styles from './styles.module.css'
 import { Tile } from '../Tile'
 import { type TerrainDifficulty } from '../../types/TerrainDifficulty'
+import { type Position } from '../../types/Position'
 import { useState } from 'react'
 import { generateGrid } from '../../utils/grid'
 import { useEffect } from 'react'
+import { Agent } from '../Agent'
+import { generateAgentPosition } from '../../utils/agent'
 
 interface GridProps{
     lines: number
@@ -12,14 +15,24 @@ interface GridProps{
 
 export function Grid({lines, columns}: GridProps){
     const [grid, setGrid] = useState<TerrainDifficulty[][]>(generateGrid(lines, columns))
+    const [agentPosition, setAgentPosition] = useState<Position>(generateAgentPosition(lines, columns, grid))
 
     useEffect(() => {
-        setGrid(generateGrid(lines, columns));
+        const newGrid = generateGrid(lines, columns);
+
+        const newAgentPosition = generateAgentPosition(lines, columns, newGrid);
+
+        setGrid(newGrid);
+        setAgentPosition(newAgentPosition);
     }, [lines, columns]);
 
     const gridTile = grid.map((row, y)=>(
         row.map((cell, x) => (
-            <Tile tipo={cell} key={`cell-${x}-${y}`} />
+            <>
+                <Tile tipo={cell} key={`cell-${x}-${y}`}>
+                    {agentPosition.y === y && agentPosition.x === x && <Agent />}
+                </Tile>
+            </>
         ))
     ))
 
--- src/component/Tile/index.tsx
+++ src/component/Tile/index.tsx
@@ -1,14 +1,17 @@
 import styles from './styles.module.css'
 import { type TerrainDifficulty } from '../../types/TerrainDifficulty'
+import type { ReactNode } from 'react'
 
 interface TileProps{
-    tipo: TerrainDifficulty
+    tipo: TerrainDifficulty,
+    children?: ReactNode
 };
 
-export function Tile({tipo}: TileProps){
+export function Tile({tipo, children}: TileProps){
     return(
         <div className={styles.tile_container}>
             {tipo}
+            {children}
         </div>
     )
         
