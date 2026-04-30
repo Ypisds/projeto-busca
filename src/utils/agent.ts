71667258d4241315beecc20239c35a01dbf1689b
Ypisds <thrbritto4@gmail.com>
2026-04-29 22:43:55 -0300

    feat(Agent): adição de funções auxiliares para a movimentação do agente

--- src/utils/agent.ts
+++ src/utils/agent.ts
@@ -17,3 +17,54 @@ export function generateAgentPosition(rows: number, columns: number, grid: Terra
     }while(grid[position.y][position.x] === 'O')
     return position
 }
+
+export function moveRight( columns: number, agentPosition: Position, grid: TerrainDifficulty[][]): Position
+{
+    const nextX = agentPosition.x + 1
+
+    if(nextX >= columns) return agentPosition;
+    if(grid[agentPosition.y][agentPosition.x + 1] === 'O') return agentPosition;
+    return {
+        x: nextX,
+        y: agentPosition.y
+    }
+}
+
+export function moveLeft(agentPosition: Position, grid: TerrainDifficulty[][]): Position 
+{
+    const nextX = agentPosition.x - 1;
+
+    if (nextX < 0) return agentPosition;
+    if (grid[agentPosition.y][nextX] === 'O') return agentPosition;
+
+    return {
+        x: nextX,
+        y: agentPosition.y
+    };
+}
+
+export function moveUp(agentPosition: Position, grid: TerrainDifficulty[][]): Position 
+{
+    const nextY = agentPosition.y - 1;
+
+    if (nextY < 0) return agentPosition;
+    if (grid[nextY][agentPosition.x] === 'O') return agentPosition;
+
+    return {
+        x: agentPosition.x,
+        y: nextY
+    };
+}
+
+export function moveDown(rows: number, agentPosition: Position, grid: TerrainDifficulty[][]): Position 
+{
+    const nextY = agentPosition.y + 1;
+
+    if (nextY >= rows) return agentPosition;
+    if (grid[nextY][agentPosition.x] === 'O') return agentPosition;
+
+    return {
+        x: agentPosition.x,
+        y: nextY
+    };
+}
